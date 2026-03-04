e3be2652c8e8d5c0f0a0d91e74cae274083c736a
Kristof Neirynck <kristof.neirynck@iqgeo.com>
2026-03-04 09:57:49 +0100

    oops

--- day01/day_1_part_2.py
+++ day01/day_1_part_2.py
@@ -11,9 +11,9 @@ def main():
     # path = Path(__file__).with_name("example.txt")
     path = Path(__file__).with_name("input.txt")
     with open(path, encoding="utf-8") as f:
-        times_pointing_at_zero_sub_total = 0
         for line in f:
             line = line.strip()
+            times_pointing_at_zero_sub_total = 0
             direction = line[0]
             if direction not in ("L", "R"):
                 raise ValueError(f"Invalid direction: {direction!r}")
@@ -22,9 +22,9 @@ def main():
             for _ in range(amount):
                 current = (current + increment) % (maximum + 1)
                 times_pointing_at_zero_sub_total += (current == zero)
-        times_pointing_at_zero += times_pointing_at_zero_sub_total
-        print(f"The dial is rotated {line} to point at {current}; "
-              f"during this rotation, it points at {zero} {times_pointing_at_zero_sub_total} times.")
+            print(f"The dial is rotated {line} to point at {current}; "
+                  f"during this rotation, it points at {zero} {times_pointing_at_zero_sub_total} times.")
+            times_pointing_at_zero += times_pointing_at_zero_sub_total
     print(f"The dial points at {zero} a total of {times_pointing_at_zero} times.")
 
 
