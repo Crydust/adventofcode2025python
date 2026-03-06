1deed254e6eb05fc710aaf55920a27ceb690ac83
Kristof Neirynck <Crydust@users.noreply.github.com>
2026-03-06 23:00:46 +0100

    more idiomatic

--- day02/day_2_part_1.py
+++ day02/day_2_part_1.py
@@ -1,49 +1,40 @@
 #!/usr/bin/env python3
 import re
-from pathlib import Path
-from typing import Any
 from dataclasses import dataclass
+from pathlib import Path
 
 
 def main():
     # path = Path(__file__).with_name("example.txt")
     path = Path(__file__).with_name("input.txt")
     product_id_ranges = read_product_id_ranges(path)
-    print(product_id_ranges)
     invalid_id_pattern = re.compile(r"(.{1,9})\1")
-    invalid_id_sum = 0
-    for product_id_range in product_id_ranges:
-        sub_list_of_invalid_ids = []
-        for i in range(product_id_range.first_id_int(), product_id_range.last_id_int() + 1):
-            s = str(i)
-            if invalid_id_pattern.fullmatch(s):
-                sub_list_of_invalid_ids.append(s)
-                invalid_id_sum += i
-        print(f"{product_id_range} has {len(sub_list_of_invalid_ids)} invalid IDs, {sub_list_of_invalid_ids}.");
-    print(f"Adding up all the invalid IDs in this example produces {invalid_id_sum}.")
+    total_invalid_sum = 0
+    for item in product_id_ranges:
+        sub_list_of_invalid_ids = [
+            i for i in range(item.first_id, item.last_id + 1)
+            if invalid_id_pattern.fullmatch(str(i))
+        ]
+        total_invalid_sum += sum(sub_list_of_invalid_ids)
+        print(f"{item} has {len(sub_list_of_invalid_ids)} invalid IDs, {sub_list_of_invalid_ids}.")
+    print(f"Adding up all the invalid IDs in this example produces {total_invalid_sum}.")
 
 
-def read_product_id_ranges(path: Path) -> list[Any]:
+def read_product_id_ranges(path: Path) -> list[ProductIdRange]:
+    content = path.read_text(encoding="utf-8").strip()
     product_id_ranges = []
-    with open(path, encoding="utf-8") as f:
-        for pair in f.readline().strip().split(","):
-            first_id, last_id = pair.split("-")
-            product_id_ranges.append(ProductIdRange(first_id, last_id))
+    for pair in content.split(","):
+        first_id, last_id = pair.split("-")
+        product_id_ranges.append(ProductIdRange(int(first_id), int(last_id)))
     return product_id_ranges
 
 
+@dataclass(frozen=True)
 class ProductIdRange:
-    def __init__(self, first_id: str, last_id: str):
-        self.first_id = first_id
-        self.last_id = last_id
+    first_id: int
+    last_id: int
 
-    def first_id_int(self) -> int:
-        return int(self.first_id)
-
-    def last_id_int(self) -> int:
-        return int(self.last_id)
-
-    def __repr__(self) -> str:
+    def __str__(self) -> str:
         return f"{self.first_id}-{self.last_id}"
 
 
