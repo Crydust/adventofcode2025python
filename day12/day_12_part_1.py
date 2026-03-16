d55cbbba8835c600f866c76cd783b0d6314f701b
Kristof Neirynck <kristof.neirynck@iqgeo.com>
2026-03-16 15:30:20 +0100

    more idiomatic

--- day12/day_12_part_1.py
+++ day12/day_12_part_1.py
@@ -2,35 +2,34 @@
 import re
 from pathlib import Path
 
+SHAPE_INDEX_PATTERN = re.compile(r"(\d):")
+SHAPE_LINE_PATTERN = re.compile(r"[.#]{3}")
+REGION_PATTERN = re.compile(r"(\d+)x(\d+): (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)")
+
 
 def main() -> None:
     # path = Path(__file__).with_name("example.txt")
     path = Path(__file__).with_name("input.txt")
-    shape_index_pattern = re.compile(r"(\d):")
-    shape_line_pattern = re.compile(r"[.#]{3}")
-    region_pattern = re.compile(r"(\d+)x(\d+): (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)")
     with path.open(encoding="utf-8") as file:
-        shapes = [0, 0, 0, 0, 0, 0]
+        shape_areas = [0, 0, 0, 0, 0, 0]
         shape_index = 0
-        areas_that_can_fit_presents = 0
+        fitting_regions = 0
         for raw_line in file:
             line = raw_line.strip()
-            if match := shape_index_pattern.fullmatch(line):
+            if match := SHAPE_INDEX_PATTERN.fullmatch(line):
                 shape_index = int(match.group(1))
-            elif shape_line_pattern.fullmatch(line):
-                count = 0
-                for ch in line:
-                    if ch == "#":
-                        count += 1
-                shapes[shape_index] += count
-            elif match := region_pattern.fullmatch(line):
-                area = int(match.group(1)) * int(match.group(2))
-                totalNeeded = sum(
-                    int(match.group(i + 3)) * (shapes[i]) for i in range(0, 6)
+            elif SHAPE_LINE_PATTERN.fullmatch(line):
+                shape_areas[shape_index] += line.count("#")
+            elif match := REGION_PATTERN.fullmatch(line):
+                width, height, *region_counts = list(map(int, match.groups()))
+                area = width * height
+                required_area = sum(
+                    count * shape_area
+                    for count, shape_area in zip(region_counts, shape_areas)
                 )
-                if area >= totalNeeded:
-                    areas_that_can_fit_presents += 1
-    print(f"{areas_that_can_fit_presents} areas can fit all the presents.")
+                if area >= required_area:
+                    fitting_regions += 1
+    print(f"{fitting_regions} areas can fit all the presents.")
 
 
 if __name__ == "__main__":
